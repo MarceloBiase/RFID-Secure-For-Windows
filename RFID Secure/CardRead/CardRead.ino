b4a809694584ce8dc041a5e711506c1b6b88b8ed
MarceloBiase <32717378+MarceloBiase@users.noreply.github.com>
2017-10-16 10:56:07 -0200

    Working with up-to-date MFRC522 library
    Working with up-to-date MFRC522.cpp - Arduino IDE Library

--- RFID Secure/CardRead/CardRead.ino
+++ RFID Secure/CardRead/CardRead.ino
@@ -25,17 +25,24 @@ char endTerm = '\n';
 char serRead[15];
 int count;
 bool auth = false;
+String stringUid = ""; 
+int stringUid_length = 4;
+byte readCard[4];   // Stores scanned ID read from RFID Module
 
 void setup() {
   Serial.begin(9600);		// Initialize serial communications with the PC
-  while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+//  while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
   SPI.begin();			// Init SPI bus
   mfrc522.PCD_Init();		// Init MFRC522
 }
 
 void loop() {
-  if (Serial.available())
-  {
+
+  delay(50); 
+  mfrc522.PCD_AntennaOn();
+// if (Serial.available())  // Serial was never available
+//  {
+    
     Read();
     if (strcmp(serRead, "init") == 0)
     {
@@ -48,9 +55,9 @@ void loop() {
     }
     else if (strcmp(serRead, "get") == 0)
     {
-      if (auth && mfrc522.stringUid.length())
+      if (auth && stringUid_length)
       {
-        Write(mfrc522.stringUid);
+        Write(stringUid);
       }
     }
     else if (strcmp(serRead, "auth") == 0)
@@ -60,16 +67,16 @@ void loop() {
     else if (strcmp(serRead, "reset") == 0)
     {
       auth = false;
-      mfrc522.stringUid = "";
+      stringUid = "";
     }
     else if (strcmp(serRead, "check") == 0)
     {
-      Write(String(checkTag()));
+
+        Write(String(checkTag()));
     }
-  }
-  
-  if (checkTag())
-    mfrc522.GetUid(&(mfrc522.uid), false);
+
+    mfrc522.PCD_AntennaOff(); // So the card can be read as new 
+    delay(50); 
 }
 
 void Write(String data)
@@ -88,8 +95,8 @@ void Read()
   char val;
   while (true)
   {
-    if (Serial.available())
-    {
+ //   if (Serial.available())
+ //   {
       val = Serial.read();
       if (val > 0)
       {
@@ -99,17 +106,46 @@ void Read()
         serRead[count] = val;
         count++;
       }
-    }
+  //  }
   }
+
+  
 }
 
 bool checkTag()
 {
+  
   bool conn = false;
-  if (mfrc522.PICC_IsNewCardPresent())
-  {
-    conn = mfrc522.PICC_ReadCardSerial();
-    mfrc522.PICC_IsNewCardPresent();
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {  
+    return false;
   }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {    
+    return false;
+  }
+
+  conn = true; 
+  stringUid = ""; 
+  // Dump debug info about the card; PICC_HaltA() is automatically called
+ // mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+
+//  Serial.println(F("Scanned PICC's UID:")); 
+  for ( uint8_t i = 0; i < 4; i++) {  //
+    readCard[i] = mfrc522.uid.uidByte[i]; 
+      if( readCard < 0x10)
+      stringUid += F("0");
+    else
+      stringUid += F("");
+    stringUid += String( readCard[i], HEX);
+       
+ //   Serial.print(readCard[i], HEX); 
+  }
+//  Serial.println("");
+  mfrc522.PICC_HaltA(); // Stop reading
+
+
+  
   return conn;
 }
